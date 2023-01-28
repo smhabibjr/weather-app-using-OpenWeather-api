7716a7e5589ad62371c9b5a0f4df748b34b30566
HabibJR <smhabib.abir2030@gmail.com>
2023-01-28 12:58:28 +0100

    add city picker

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -3,6 +3,60 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="weather"
 export default class extends Controller {
   connect() {
-    console.log("weather controller has been connected!");
+    $("#select_country").off().change(function (e) {
+      e.preventDefault();
+      const country = {
+        country: $(this).val()
+      };
+      $.ajax({
+        type: "POST",
+        url: "/city",
+        data: country,
+        async: false,
+        success: function (response) {
+          $('#select_state').empty().append('<option value="">Select State</option>')
+          $.each(response, function( index, value ) {
+            $('#select_state').append(`<option value="${index}">
+                                       ${value}
+                                  </option>`);
+
+          });
+        },
+        error: function(response){
+        }
+      });
+    });
+
+
+    $("#select_state").off().change(function (e) {
+      e.preventDefault();
+      const state = {
+        selected_country: $("#select_country").val(),
+        state: $(this).val()
+      };
+      $.ajax({
+        type: "POST",
+        url: "/city",
+        data: state,
+        async: false,
+        success: function (response) {
+          $('#select_city').empty().append('<option value="">Select State</option>')
+          $.each(response, function( index, value ) {
+            $('#select_city').append(`<option value="${value}">
+                                       ${value}
+                                  </option>`);
+
+          });
+        },
+        error: function(response){
+        }
+      });
+    });
+
+    $("#select_city").off().change(function (e){
+      e.preventDefault();
+      $("#city_name").val($(this).val());
+    });
+
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'city#index'
   resources :weather, only: [:index, :create,]
-  #root 'weather#index'
+  root 'weather#index'
   get 'weather/show' => "weather#show"
-  resources :city, only: [:index, :create]
+  resources :city, only: [:create]
 end
